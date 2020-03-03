da12808d5bc64d0e3e9f8798f72ef50a5ee6ddfe
rewalczak <rewalczak@gmail.com>
2020-03-03 13:06:51 -0600

    Add files via upload

---
+++ test_collections/test_sort_and_search_list.py
@@ -0,0 +1,19 @@
+"""Robert Walczak"""
+
+import unittest
+from fun_with_collections import sort_and_search_list
+
+
+class SearchList(unittest.TestCase):
+    def test_search_list_for_item_found(self):
+        self.assertEqual(sort_and_search_list.search_list([5, 10, 15, 20, 25], 25), 4)
+
+    def test_search_list_for_item__not_found(self):
+        self.assertEqual(sort_and_search_list.search_list([5, 10, 15, 20, 25], 30), -1)
+
+    def test_sort_list(self):
+        self.assertEqual(sort_and_search_list.sort_list([]), None)
+
+
+if __name__ == '__main__':
+    unittest.main()
